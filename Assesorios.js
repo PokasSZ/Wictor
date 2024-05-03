3eedcb325ee167207d5d2931b89b4d79b38bc46f
PokasSZ <wictorvieira1213@gmail.com>
2024-05-03 15:28:57 -0300

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   Aparencia.css
    	new file:   Assesorios.js
    	new file:   Cadeira.jpg
    	new file:   Esqueleto .html
    	new file:   Gabinete.webp
    	new file:   Monitor360Hz.jpg
        new file:   MousePad.png
    	new file:   Noot.jpg
    	new file:   Processador.jpg
    	new file:   TecladoBranco.jpg
    	new file:   apexpro.jpg
    	new file:   carrinho.css
    	new file:   carrinho.html
    	new file:   mouse.jpg
    	new file:   placadevideo.webp
    
     Untracked files:
    	aula .py

---
+++ Assesorios.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const produtos = [
+        { nome: "Placa de Vídeo NVIDIA GeForce RTX 4090", preco: "R$ 10.899,00", imagem: "placadevideo.webp" },
+        { nome: "Teclado Apex Pro Sem Fio", preco: "R$ 1.399,00", imagem: "apexpro.jpg" },
+        { nome: "Mouse Gamer", preco: "R$ 799,00", imagem: "mouse.jpg" }, 
+        { nome: "Mouse Pad Hype X", preco: "R$ 299,00", imagem: "MousePad.png" },
+        { nome: "Processador Intel Core i9-13900K", preco: "R$ 4.299,00", imagem: "Processador.jpg" },
+        { nome: "GABINETE GAMER LIAN LI - REDRAGON MODELO O11 DYNAMIC BRANCO", preco: "R$ 859,99", imagem: "Gabinete.webp" },
+        { nome: "Monitor Gamer Duex, 27 Pol, Full HD, IPS, 360Hz, 1ms, HDR", preco: "R$ 1.899,00", imagem: "Monitor360Hz.jpg" },
+        { nome: "Notebook Gamer Acer Nitro 5 AN517-54-765V Intel core I7 11ª ger. 16GB RAM 512GB SSD (NVIDIA RTX 3050) 17,3 LED FHD IPS 144Hz Linux Gutta Preto com vermelho", preco: "R$ 4.799,00", imagem: "Noot.jpg" },
+        { nome: "Cadeira Gamer Fair Play", preco: "R$ 899,00", imagem: "Cadeira.jpg" },
+        { nome: "Teclado Gamer, Razer Huntsman Mini 60%", preco: "R$ 699,00", imagem: "TecladoBranco.jpg" },
+    ];
+
+    const containerProdutos = document.getElementById("produtos");
+
+    produtos.forEach((produto, index) => {
+        const produtoHTML = `
+            <div class="produto">
+                <img src="${produto.imagem}" alt="${produto.nome}">
+                <h2>${produto.nome}</h2>
+                <p class="preco">${produto.preco}</p>
+                <button onclick="adicionarAoCarrinho(${index})">Adicionar ao Carrinho</button>
+            </div>
+        `;
+        containerProdutos.innerHTML += produtoHTML;
+    });
+});
+
+function adicionarAoCarrinho(index) {
+    // Redireciona para a página do carrinho com o índice do produto como parâmetro
+    window.location.href = `carrinho.html?produto=${index}`;
+}
+
